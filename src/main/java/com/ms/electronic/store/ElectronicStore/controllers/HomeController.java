af21847575367fb31e90b5eadeb8ca8bedbd68e1
sengarmadhur <madhursengar24@gmail.com>
2024-08-24 23:11:44 +0530

    SpringBoot first commit

---
+++ src/main/java/com/ms/electronic/store/ElectronicStore/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.ms.electronic.store.ElectronicStore.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    @GetMapping
+    public String test() {
+        return "Hello";
+    }
+}

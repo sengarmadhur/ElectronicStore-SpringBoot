fb211cc82d08c03f9c9d427ad49a389cd1dc8004
sengarmadhur <madhursengar24@gmail.com>
2024-08-26 01:00:38 +0530

    Creating User Entity, User Service and User Repository

---
+++ src/main/java/com/ms/electronic/store/ElectronicStore/entities/User.java
@@ -0,0 +1,38 @@
+package com.ms.electronic.store.ElectronicStore.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_password", length = 10)
+    private String password;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+    private String gender;
+
+    @Column(length = 100)
+    private String about;
+
+    @Column(name = "user_image_name")
+    private String imageName;
+}
---
+++ src/main/java/com/ms/electronic/store/ElectronicStore/services/UserService.java
@@ -0,0 +1,15 @@
+package com.ms.electronic.store.ElectronicStore.services;
+
+import com.ms.electronic.store.ElectronicStore.entities.User;
+
+public interface UserService {
+    //create
+    User createUser(User user) ;
+    //update
+    //delete
+    //get all users
+    //get single user by id
+    //get single user by email
+    //search user
+    //other user specific features
+}
